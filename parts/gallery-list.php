b184080df8346ad40402587ec3e86a4d00098aaa
Craig Fowler <craig@wearegiants.com>
2015-10-13 15:03:21 -0700

    junk

--- parts/gallery-list.php
+++ parts/gallery-list.php
@@ -17,14 +17,27 @@
    $orient = 'medium';
 
    }
+
+   $link = get_field('wide_image', $image['ID']);
+
+   if ($link) {
+      $itemWidth = 'fs-all-full full';
+   } else {
+      $itemWidth = 'fs-all-half';
+   }
+
+   if ($link) {
+      $bgHeight = 'full';
+   }
+
 ?>
 
 
 <?php if(!$image['caption']): ?>
 
-<div class="gallery--list_item fs-cell fs-all-half fs-contained">
+<div class="gallery--list_item fs-cell <?php echo $itemWidth; ?> fs-contained">
    <a href="<?php echo $image['sizes']['large']; ?>" class="lightbox lightbox_image gallery--zoom" data-lightbox-gallery="gallery">
-      <span class="gallery--list_item_bg background <?php echo $orient; ?>" data-background-options='{"source":{"0px":"<?php echo $image['sizes']['medium']; ?>","980px":"<?php echo $image['sizes']['large']; ?>"}}'></span>
+      <span class="gallery--list_item_bg background <?php echo $orient; echo ' '; echo $bgHeight; ?>" data-background-options='{"source":{"0px":"<?php echo $image['sizes']['medium']; ?>","980px":"<?php echo $image['sizes']['large']; ?>"}}'></span>
    </a>
 </div>
 
@@ -40,7 +53,7 @@
 
 <div class="gallery--list_item description fs-cell fs-all-half fs-contained">
    <div class="wrapper text-center">
-      <?php echo $image['title']; ?>
+      <h4><?php echo $image['title']; ?></h4>
       <?php echo $image['caption']; ?>
    </div>
    <span class="gallery--list_item_bg <?php echo $orient; ?>"></span>
